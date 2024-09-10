5bf02c1b63b5aa661cabfe669e9e4810cb521c77
alexeiepif <aled2093746@gmail.com>
2024-09-10 19:00:19 +0300

    modify ind2 and add test version

--- program/ind_2.py
+++ program/ind_2.py
@@ -51,7 +51,87 @@ class Account:
 
     def amount_in_words(self):
         # Реализация преобразования суммы в числительное
-        pass
+        sl_n = [
+            {
+                0: "ноль",
+                1: "один",
+                2: "два",
+                3: "три",
+                4: "четыре",
+                5: "пять",
+                6: "шесть",
+                7: "семь",
+                8: "восемь",
+                9: "девять",
+            },
+            {
+                1: "десять",
+                2: "двадцать",
+                3: "тридцать",
+                4: "сорок",
+                5: "пятьдесят",
+                6: "шестьдесят",
+                7: "семьдесят",
+                8: "восемьдесят",
+                9: "девяносто",
+            },
+            {
+                1: "сто",
+                2: "двести",
+                3: "триста",
+                4: "четыреста",
+                5: "пятьсот",
+                6: "шестьсот",
+                7: "семьсот",
+                8: "восемьсот",
+                9: "девятьсот",
+            },
+            {
+                1: "тысяча",
+                2: "две тысячи",
+                3: "три тысячи",
+                4: "четыре тысячи",
+                5: "пять тысяч",
+                6: "шесть тысяч",
+                7: "семь тысяч",
+                8: "восемь тысяч",
+                9: "девять тысяч",
+            },
+            {
+                1: "одиннадцать",
+                2: "двенадцать",
+                3: "тринадцать",
+                4: "четырнадцать",
+                5: "пятнадцать",
+                6: "шестнадцать",
+                7: "семнадцать",
+                8: "восемнадцать",
+                9: "девятнадцать",
+            },
+        ]
+
+        bal = list(map(int, str(self.__balance)))
+        bal.reverse()
+        list_bal = []
+        if len(bal) == 1:
+            str_bal = sl_n[bal[0]]
+        elif len(bal) < 5:
+            prew = 0
+            for count, i in enumerate(bal):
+                if (count == 1) and (i == 1) and (prew != 0):
+                    list_bal[0] = sl_n[-1][prew]
+                else:
+                    val = sl_n[count].get(i, None)
+                    if val:
+                        list_bal.append(val)
+                prew = i
+            list_bal.reverse()
+            str_bal = " ".join(list_bal)
+        else:
+            print("Сумма больше 99999")
+            return None
+
+        return str_bal
 
     def display(self):
         print(f"Владелец счета: {self.__owner}")
@@ -61,15 +141,17 @@ class Account:
 
 # Демонстрация возможностей класса
 if __name__ == "__main__":
-    my_account = Account("Иванов", "10032", 0.05, 1000)
+    my_account = Account("Иванов", "10032", 0.05, 9045)
     my_account.display()
-    my_account.change_owner("Петров")
-    my_account.deposit(500)
-    my_account.withdraw(300)
-    my_account.add_interest()
-    print("\nИзменённый счет:\n")
-    my_account.display()
-    usd_amount = my_account.convert_to_usd()
-    print(f"Баланс в USD: ${usd_amount}")
-    eur_amount = my_account.convert_to_eur()
-    print(f"Баланс в EUR: €{eur_amount}")
+    # my_account.change_owner("Петров")
+    # my_account.deposit(500)
+    # my_account.withdraw(300)
+    # my_account.add_interest()
+    # print("\nИзменённый счет:\n")
+    # my_account.display()
+    # usd_amount = my_account.convert_to_usd()
+    # print(f"Баланс в USD: ${usd_amount}")
+    # eur_amount = my_account.convert_to_eur()
+    # print(f"Баланс в EUR: €{eur_amount}")
+    word = my_account.amount_in_words()
+    print(f"Сумма в рублях: {word}")
